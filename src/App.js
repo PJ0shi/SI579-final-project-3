44b1bd9d0337a8f0a400d012e84101c60914f16c
PJ0shi <122579354+PJ0shi@users.noreply.github.com>
2023-12-06 23:28:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// In App.js
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Header from "./Header.js";
@@ -10,29 +9,19 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [loggedInUser, setLoggedInUser] = useState('');
+  const [users, setUsers] = useState([]); //Array of register users
+  const [loggedInUser, setLoggedInUser] = useState(''); //Username of the currently logged-in user
   const navigate = useNavigate(); // Hook for navigation
 
   const handleLogin = (enteredUsername, enteredPassword) => {
     // Retrieve the users array from local storage
     const storedUsers = localStorage.getItem('users');
-
-    // Parse the storedUsers string to convert it back to an array
     const users = storedUsers ? JSON.parse(storedUsers) : [];
 
     // Check for authentication using the entered username and password
     const authenticatedUser = users.some((user) => user.username === enteredUsername && user.password === enteredPassword);
     
-    console.log("authenticatedUser = " + authenticatedUser)
-    console.log("enteredUsername = " + enteredUsername)
-    console.log("enteredPassword = " + enteredPassword)
-    console.log("users array = " + users)
-
-
     if (authenticatedUser) {
-      setIsAuthenticated(true);
       setLoggedInUser(enteredUsername);
       //insert navigation to feedback form
       navigate('/feedback');
@@ -42,25 +31,19 @@ function App() {
   };
 
   const handleLogout = () => {
-    setIsAuthenticated(false);
     setLoggedInUser('');
     navigate('/');  
   };
 
-
-  const [currentForm, setcurrentForm] = useState("login")
   return (
     <div>
       <Header onLogout={handleLogout}/>
-
-      {/* Define routes */}
       <Routes>
-  <Route path="/" element={<Login onLogin={handleLogin} />} />
-  <Route path="/register" element={<Register onAuthentication={setUsers} />} />
-  <Route path="/feedback" element={<FeedbackForm username={loggedInUser} onLogout={handleLogout} />} />
-</Routes>
-
-    <Footer />
+        <Route path="/" element={<Login onLogin={handleLogin} />} />
+        <Route path="/register" element={<Register onAuthentication={setUsers} />} />
+        <Route path="/feedback" element={<FeedbackForm username={loggedInUser} onLogout={handleLogout} />} />
+      </Routes>  
+      <Footer />
     </div>
   );
 }
